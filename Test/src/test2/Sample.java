aa522464077e365e86906bcb0870f40f4d65560a
nikhilpurwant8791 <purwantn@gmail.com>
2021-10-03 22:11:33 +0530

    Second Commit

--- Test/src/test2/Sample.java
+++ Test/src/test2/Sample.java
@@ -2,7 +2,9 @@ package test2;
 
 public class Sample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello JAVA");
 		// TODO Auto-generated method stub
 
 	}
